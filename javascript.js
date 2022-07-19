4df9eb57afee831373da7e9a8d3f47151479f375
andrezaaraujo <andreza.si.araujo@gmail.com>
2022-07-18 20:00:40 -0400

    Alterações projeto Tera

---
+++ javascript.js
@@ -0,0 +1,27 @@
+//let count = 1;
+
+//setInterval(function(){
+  //  document.getElementById("img")
+   // count++
+//if (count >3){
+ //   count = 1;
+//}
+
+//},3000)
+console.log("funcionando")
+
+ let test = document.getElementById("teste");
+
+ test.addEventListener("mouseover", function (event) {
+  
+  event.target.style.color = "black";
+ }, false);
+
+ test.addEventListener("mouseout", function (event) {
+  
+  event.target.style.color = "green";
+ }, false);
+
+function alerta(){
+alert("Nenhum cadastro realizado!")
+}
